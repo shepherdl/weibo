2aa17dd8320481f50a7a805578cb8bb7a73db184
Shepherd <shepherd.lyq@gmail.com>
2022-10-28 14:42:49 +0000

    登录成功后导航逻辑

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Auth;
+
 class SessionsController extends Controller
 {
     public function create()
